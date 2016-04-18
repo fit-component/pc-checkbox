438242c543c44a5e8be530a02a47d759ba49445c
huangziyi01 <huangziyi01@baidu.com>
2016-04-18 09:15:33 +0800

    quick push

--- demo/lists/label.js
+++ demo/lists/label.js
@@ -5,7 +5,7 @@ export default class Demo extends React.Component {
     render() {
         return (
             <Checkbox label="我爱fit"
-                      labelWidth="100">点击选中</Checkbox>
+                      labelWidth={100}>点击选中</Checkbox>
         )
     }
 }
